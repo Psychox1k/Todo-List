047b131658fe7c60bb4a82ec888cf76fe7192a48
Psychox1k <success01001011@gmail.com>
2025-09-09 20:26:09 +0200

    feat(todo): create CreateView for Tag and Task models, add path to layouts,create TaskForm

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from todo.views import TaskListView, TagListView
+from todo.views import TaskListView, TagListView, TaskCreateView, TagCreateView
+
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
 ]
 
 app_name = "todo"
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
+from todo.forms import TaskForm
 from todo.models import Task, Tag
 
 
@@ -9,7 +11,19 @@ class TaskListView(generic.ListView):
     template_name = "todo/index.html"
     paginate_by = 10
 
+
 class TagListView(generic.ListView):
     model = Tag
     paginate_by = 10
 
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag-list")
