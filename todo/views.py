3a27f61d69dbca7600962b54426707770b6b1c43
Psychox1k <success01001011@gmail.com>
2025-09-09 18:01:42 +0200

    feat(todo): Add URL configuration and views for Task and Tag List
    - Configure URLs for the todo app
    - Implement Task List View
    - Implement Tag List View

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from todo.models import Task, Tag
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    template_name = "todo/index.html"
+
+class TagListView(generic.ListView):
+    model = Tag
+
